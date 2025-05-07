c9cfe4bbffcf484fd8009560013d6681fa36b83b
Emilis <lecitrey@gmail.com>
2025-05-08 01:24:05 +0200

    Create welcome.lua

---
+++ welcome.lua
@@ -0,0 +1,13 @@
+--[[
+Thank you for using Veltra!
+
+This executor was made purley for simplicity and performance.
+Why use Veltra? Veltra is super useful for simple exploitation,
+farming, testing and more!
+
+Official Discord: https://discord.gg/dB5wau3k5D
+Official Website: https://getveltra.vercel.app/
+
+Veltra is based on Xeno's API.
+Want to use Xeno? Visit https://www.xeno.now/
+]]
